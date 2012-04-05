3205f9d9c8733fc3e718ac6cf5ee4f2dbe8df5ce
Jerome Schneider <schnej7@rpi.edu>
2012-04-05 15:26:28 -0400

    is_installed function implimented

--- errspeak.c
+++ errspeak.c
@@ -1,9 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int is_installed( char * program_name ){
   char * my_path = getenv( "PATH" );
-  printf("%s\n", my_path );
+
+  char * a_path = strtok ( my_path, ":" );
+  
+  FILE * fd;
+  while( 1 ){
+    fd = NULL;
+    char * full_path = (char *) malloc( 1 + strlen(program_name) + strlen(a_path) );
+
+    int i;
+    for( i = 0; i < strlen(a_path); i++ ){
+      full_path[i] = a_path[i];
+    }
+    full_path[i] = '/';
+    for( i = 0; i < strlen(program_name); i++ ){
+      full_path[strlen(a_path) + i + 1] = program_name[i];
+    }
+
+    fd = fopen( full_path, "r" );
+    if( !fd ){
+    }
+    else{
+      close( fd );
+      return 1;
+    }
+    a_path = strtok ( NULL, ":" );
+
+    if( a_path == NULL ){
+       break;
+    }
+  }
   return 0;
 }
 
--- test.c
+++ test.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "errspeak.h"
 
-int main(){
-  is_installed( "espeak" );
+int main( int argv, char * argc[] ){
+  if( argv <= 1 ) return EXIT_FAILURE;
+  if (is_installed( argc[1] ) ){
+    printf("%s is installed\n", argc[1]);
+  }
+  else{
+    printf("%s is not installed\n", argc[1]);
+  }
   return EXIT_SUCCESS;
 }
 
